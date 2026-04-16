3cbd169e07a074922d1844a272af9b21f4be455c
Xènia F <xenia.fabrega01@estudiant.upf.edu>
2026-04-16 17:43:15 +0200

    Add edit button!
    Co-authored-by: Margarita Crespo Villaverde <MargaritaCrespoUni@users.noreply.github.com>

--- src/main/java/epaw/lab1/HelloWorld.java
+++ src/main/java/epaw/lab1/HelloWorld.java
@@ -19,6 +19,7 @@ public class HelloWorld extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+        String editID = request.getParameter("editID");
         out.println("<h1>Hola des del Servlet!</h1>");
         
         out.println("<!DOCTYPE html>");
@@ -47,12 +48,21 @@ public class HelloWorld extends HttpServlet {
                 out.println("<td>" + description + "</td>");
 
                 out.println("<td>");
+                
+                //edit
+                out.println("<form method='GET' action='/hello' style='display:inline;'>");
+                out.println("<input type='hidden' name='editID' value='" + id + "'>");
+                out.println("<button type='submit'>✏️</button>");
+                out.println("</form>");
+                
+                //delete
                 out.println("<form method='POST' action='/hello' style='display:inline;'>");
                 out.println("<input type='hidden' name='action' value='delete'>");
                 out.println("<input type='hidden' name='id' value='" + id + "'>");
                 out.println("<button type='submit'>🗑️</button>");
                 out.println("</form>");
-
+                
+                out.println("</td>");
                 out.println("</tr>");
             }
         } catch (Exception e) {
@@ -62,11 +72,40 @@ public class HelloWorld extends HttpServlet {
 
         out.println("</table>");
 
-        out.println("<h2>Add new user</h2>");
+        String editName = "";
+        String editDescription = "";
+        String idValue="";
+        boolean isEdit = false;
+
+        if (editID!=null){
+            isEdit = true;
+            try (DBManager db = new DBManager()) {
+                PreparedStatement stmt = db.prepareStatement("SELECT name, description FROM users WHERE id = ?");
+                stmt.setInt(1, Integer.parseInt(editID));
+                ResultSet rs = stmt.executeQuery();
+                
+                if (rs.next()) {
+                    editName = rs.getString("name");
+                    editDescription = rs.getString("description");
+                    idValue=editID;
+                }
+            } catch (Exception e) {
+                out.println("<p>Error fetching user for edit: " + e.getMessage() + "</p>");
+                e.printStackTrace();
+            }
+        }
+
+        out.println("<h2>" + (isEdit ? "Edit user" : "Add new user") + "</h2>");
         out.println("<form method='POST' action='/hello'>");
-        out.println("Name: <input type='text' name='name' required><br><br>");
-        out.println("Description: <input type='text' name='description' required><br><br>");
-        out.println("<button type='submit'>Add</button>");
+
+        if(isEdit){
+            out.println("<input type='hidden' name='action' value='edit'>");
+            out.println("<input type='hidden' name='id' value='" + idValue + "'>");
+        }
+        
+        out.println("Name: <input type='text' name='name' value='" + editName + "' required><br><br>");
+        out.println("Description: <input type='text' name='description' value='" + editDescription + "' required><br><br>");
+        out.println("<button type='submit'>" + (isEdit ? "Update" : "Add") + "</button>");
         out.println("</form>");
 
         out.println("</body>");
@@ -88,8 +127,22 @@ public class HelloWorld extends HttpServlet {
                     deleteStmt.setInt(1, id);
                     deleteStmt.executeUpdate();
                 }
+                else if("edit".equals(action)){
+                    int id = Integer.parseInt(request.getParameter("id"));
+                    String name = request.getParameter("name");
+                    String description = request.getParameter("description");
+                    
+                    
+                    PreparedStatement updateStmt = db.prepareStatement("UPDATE users SET name = ?, description = ? WHERE id = ?");
+                    updateStmt.setString(1, name);
+                    updateStmt.setString(2, description);
+                    updateStmt.setInt(3, id);
+                    
+                    updateStmt.executeUpdate();
+                }
+            
                 else{
-                     String name = request.getParameter("name");
+                    String name = request.getParameter("name");
                     String description = request.getParameter("description");
                     PreparedStatement stmt = db.prepareStatement("INSERT INTO users (name, description) VALUES (?, ?)");
                 
